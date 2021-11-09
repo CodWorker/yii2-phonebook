a6a5ab0e1605b285715e8df28ea37b5cdd4a69ce
coderius <sunrise4fun@gmail.com>
2021-11-09 21:20:14 +0200

    Create project

---
+++ frontend/config/urlmanager.php
@@ -0,0 +1,22 @@
+<?php
+return [
+            'class' => 'yii\web\UrlManager',
+            'baseUrl' => $baseUrl,
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => true,
+            'rules' => [
+
+                // '/' => 'site/index',
+                // '<action:(login|logout|signup|auth)>' => 'site/<action>',
+                // 'site/<action>' => 'site/<action>',
+                // 'sitemap.xml' => 'sitemap/index',
+                // 'rss.xml' => 'rss/index',
+
+                '/' => 'contacts/index',
+                '<action:(login|logout|signup|auth)>' => 'site/<action>',
+                'contacts/<action>' => 'contacts/<action>',
+                
+            ],
+        ];
+?>
---
+++ frontend/models/Contacts.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "contacts".
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $email
+ * @property string $birthday
+ * @property string $phone
+ */
+class Contacts extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'contacts';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['first_name', 'last_name', 'email', 'birthday', 'phone'], 'required'],
+            [['first_name', 'last_name', 'email', 'birthday', 'phone'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'first_name' => 'First Name',
+            'last_name' => 'Last Name',
+            'email' => 'Email',
+            'birthday' => 'Birthday',
+            'phone' => 'Phone',
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return ContactsQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new ContactsQuery(get_called_class());
+    }
+}
